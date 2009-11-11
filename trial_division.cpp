441eb193043fc795695a86a0dacc2127d4c3face
ecksun <ecksun@gmail.com>
2009-11-11 22:53:23 +0100

    Ändrade trial divison för att använda mpz_nextprime istället. Eventuellt lite långsammare (har inte märkt något än), dock mycket lättare att skala.
    De 256 första primtalen ger 34p
    Accepted (34)   60.75 s

--- trial_division.cpp
+++ trial_division.cpp
@@ -3,44 +3,46 @@
 
 
 trial_division::trial_division() {
-    divisor[0] = 3;
-    divisor[1] = 5;
-    divisor[2] = 7;
-    divisor[3] = 11;
-    divisor[4] = 13;
-    divisor[5] = 17;
-    divisor[6] = 19;
-    divisor[7] = 23;
-    divisor[8] = 29;
-    divisor[9] = 31;
-    divisor[10] = 37;
-    divisor[11] = 41;
-    divisor[12] = 43;
-    divisor[13] = 47;
-    divisor[14] = 53;
-    divisor[15] = 59;
-    divisor[16] = 61;
-    divisor[17] = 67;
-    divisor[18] = 71;
-    divisor[19] = 73;
-    divisor[20] = 79;
-    divisor[21] = 83;
-    divisor[22] = 89;
-    divisor[23] = 97;
-    divisor[24] = 101;
-    divisor[25] = 103;
-    divisor[26] = 107;
-    divisor[27] = 109;
-    divisor[28] = 113;
-    divisor[29] = 127;
-    divisor[30] = 131;
-    divisor[31] = 137;
+    // divisor[0] = 3;
+    // divisor[1] = 5;
+    // divisor[2] = 7;
+    // divisor[3] = 11;
+    // divisor[4] = 13;
+    // divisor[5] = 17;
+    // divisor[6] = 19;
+    // divisor[7] = 23;
+    // divisor[8] = 29;
+    // divisor[9] = 31;
+    // divisor[10] = 37;
+    // divisor[11] = 41;
+    // divisor[12] = 43;
+    // divisor[13] = 47;
+    // divisor[14] = 53;
+    // divisor[15] = 59;
+    // divisor[16] = 61;
+    // divisor[17] = 67;
+    // divisor[18] = 71;
+    // divisor[19] = 73;
+    // divisor[20] = 79;
+    // divisor[21] = 83;
+    // divisor[22] = 89;
+    // divisor[23] = 97;
+    // divisor[24] = 101;
+    // divisor[25] = 103;
+    // divisor[26] = 107;
+    // divisor[27] = 109;
+    // divisor[28] = 113;
+    // divisor[29] = 127;
+    // divisor[30] = 131;
+    // divisor[31] = 137;
 
 }
 
 void trial_division::factor(mpz_t n, std::vector<mpz_class> & factors) {
     // std::cerr << "factor(" << n << ")" << std::endl;
 
+    mpz_t prime;
+    mpz_init_set_ui(prime, 2);
     /*
      * In case we have found the factor "1", just return
      */
@@ -60,12 +62,13 @@ void trial_division::factor(mpz_t n, std::vector<mpz_class> & factors) {
     }
     else {
         for (int i = 0; i < divisors; i++) {
-            if (mpz_divisible_ui_p(n, divisor[i]) != 0) {
-                mpz_class factor(divisor[i]);
+            mpz_nextprime(prime, prime);
+            if (mpz_divisible_p(n, prime) != 0) {
+                mpz_class factor(prime);
 
                 factors.push_back(factor);
 
-                mpz_divexact_ui(n, n, divisor[i]);
+                mpz_divexact(n, n, prime);
                 this->factor(n, factors);
                 return;
             }
--- trial_division.h
+++ trial_division.h
@@ -3,8 +3,8 @@
 #include <vector>
 
 class trial_division {
-    const static int divisors = 32;
-    unsigned long int divisor[divisors];
+    const static int divisors = 256;
+    // unsigned long int divisor[divisors];
 
     public: 
     void factor(mpz_t, std::vector<mpz_class> &);
