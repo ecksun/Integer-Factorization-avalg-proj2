eec0c41f5abc358e50286ddc12cd373fe138993d
Joel Pettersson <petterssonjoel@gmail.com>
2009-11-21 17:16:19 +0100

    Började implementera hela fermat_squares.
    Får dock lite problem, så ska försöka refaktorisera så att allt sker i
    factorize.

--- fermat_squares.cpp
+++ fermat_squares.cpp
@@ -1,5 +1,6 @@
 #include "fermat_squares.h"
 #include <iostream>
+#define prime_test_reps 5
 
 /**
  * Factorizes the specified multiple precision integer into its prime
@@ -10,13 +11,32 @@
 bool fermat_squares::prime_factorize(mpz_t n, std::vector<mpz_class> & factors) {
 
     std::pair<mpz_class,mpz_class> factors_pair;
+    factorize(n, factors_pair);
+    // TODO: problem med överskrivning av factors_pair?
+    
+    mpz_t first;
+    mpz_init_set(first, factors_pair.first.get_mpz_t());
 
-    fermat_squares::factorize(n, factors_pair);
+    // store/recurse first factor
+    if (mpz_probab_prime_p(first, prime_test_reps)) {
+        factors.push_back(factors_pair.first);
+    } else {
+        prime_factorize(first, factors);
+    }
 
-    factors.push_back(factors_pair.first);
-    factors.push_back(factors_pair.second);
+    mpz_t second;
+    mpz_init_set(second, factors_pair.second.get_mpz_t());
 
-    return false;
+    // store/recurse second factor
+    if (mpz_probab_prime_p(second, prime_test_reps)) {
+        if (mpz_cmp(first, second)) {
+            factors.push_back(factors_pair.second);
+        }
+    } else {
+        prime_factorize(second, factors);
+    }
+
+    return true;
 }
 
 /**
@@ -68,7 +88,7 @@ bool fermat_squares::factorize(mpz_t n, std::pair<mpz_class,mpz_class> & factors
  */
 void fermat_squares::q(mpz_t & q_x, mpz_t x, mpz_t n) {
     mpz_class x_sq_cls;
-    mpz_pow_ui(x_sq_cls.get_mpz_t(), x, 2);
+    mpz_mul(x_sq_cls.get_mpz_t(), x, x);
 
     // Q(x)
     mpz_class q_x_cls; // init
